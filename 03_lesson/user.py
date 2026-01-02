7c6c64590c4ae80319e66169304237cb06209124
helga <olga.odintsova83@gmail.com>
2026-01-03 03:37:54 +0400

    add lesson3 task1

---
+++ 03_lesson/user.py
@@ -0,0 +1,13 @@
+class User:
+    def __init__(self, first_name, last_name):
+        self._first_name = first_name
+        self._last_name = last_name
+        
+    def print_first_name(self):
+        print(self._first_name)
+
+    def print_last_name(self):
+        print(self._last_name)
+
+    def print_full_name(self):
+        print(self._first_name, self._last_name)
