20877452548831e68ce8d8783749843fbe164792
helga <olga.odintsova83@gmail.com>
2026-02-04 14:48:11 +0400

    fix first task

--- 06K_lesson/test_01_form.py
+++ 06K_lesson/test_01_form.py
@@ -1,62 +1,63 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+
+url = "https://msedgedriver.microsoft.com/LATEST_RELEASE"
+driver = webdriver.Edge(
+    service=EdgeService(EdgeChromiumDriverManager(
+        url="https://msedgedriver.microsoft.com",
+        latest_release_url=url).install())
+)
+
+
+def change_input(name, value):
+    selector = 'input[name="' + name + '"]'
+    pres = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
+    WebDriverWait(driver, 4).until(pres).send_keys(value)
+
+
+def submit():
+    selector = 'button[type="submit"]'
+    pres = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
+    WebDriverWait(driver, 4).until(pres).click()
+
+
+def check_result(name, cls):
+    selector = '#' + name
+    pres = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
+    classes = WebDriverWait(driver, 4).until(pres).get_attribute('class')
+    assert cls in classes
 
 
 def test_form():
 
-    url = "https://msedgedriver.microsoft.com/LATEST_RELEASE"
-    driver = webdriver.Edge(
-        service=EdgeService(EdgeChromiumDriverManager(
-            url="https://msedgedriver.microsoft.com",
-            latest_release_url=url).install())
-    )
+    page = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+    driver.get(page)
+    change_input('first-name', "Иван")
+    change_input('last-name', "Петров")
+    change_input('address', "Ленина, 55-3")
+    change_input('city', "Москва")
+    change_input('country', "Россия")
+    change_input('e-mail', "test@skypro.com")
+    change_input('phone', "+7985899998787")
+    change_input('job-position', "QA")
+    change_input('company', "SkyPro")
 
-    driver.get(
-            "https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
-    driver.find_element(
-            By.CSS_SELECTOR, 'input[name="first-name"]').send_keys("Иван")
-    driver.find_element(
-            By.CSS_SELECTOR, 'input[name="last-name"]').send_keys("Петров")
-    driver.find_element(
-            By.CSS_SELECTOR, 'input[name="address"]').send_keys("Ленина, 55-3")
-    driver.find_element(
-            By.CSS_SELECTOR, 'input[name="city"]').send_keys("Москва")
-    driver.find_element(
-            By.CSS_SELECTOR, 'input[name="country"]').send_keys("Россия")
-    driver.find_element(
-            By.CSS_SELECTOR, 'input[name="e-mail"]'
-            ).send_keys("test@skypro.com")
-    driver.find_element(
-            By.CSS_SELECTOR, 'input[name="phone"]').send_keys("+7985899998787")
-    driver.find_element(
-            By.CSS_SELECTOR, 'input[name="job-position"]').send_keys("QA")
-    driver.find_element(
-            By.CSS_SELECTOR, 'input[name="company"]').send_keys("SkyPro")
-    driver.find_element(
-            By.CSS_SELECTOR, 'button[type="submit"]').click()
+    submit()
 
-    assert 'alert-success' in driver.find_element(
-            By.CSS_SELECTOR, '#first-name').get_attribute('class')
-    assert 'alert-success' in driver.find_element(
-            By.CSS_SELECTOR, '#last-name').get_attribute('class')
-    assert 'alert-success' in driver.find_element(
-            By.CSS_SELECTOR, '#address').get_attribute('class')
-    assert 'alert-success' in driver.find_element(
-            By.CSS_SELECTOR, '#city').get_attribute('class')
-    assert 'alert-success' in driver.find_element(
-            By.CSS_SELECTOR, '#country').get_attribute('class')
-    assert 'alert-success' in driver.find_element(
-            By.CSS_SELECTOR, '#e-mail').get_attribute('class')
-    assert 'alert-success' in driver.find_element(
-            By.CSS_SELECTOR, '#phone').get_attribute('class')
-    assert 'alert-success' in driver.find_element(
-            By.CSS_SELECTOR, '#job-position').get_attribute('class')
-    assert 'alert-success' in driver.find_element(
-            By.CSS_SELECTOR, '#company').get_attribute('class')
-
-    assert 'alert-danger' in driver.find_element(
-            By.CSS_SELECTOR, '#zip-code').get_attribute('class')
+    check_result('first-name', 'alert-success')
+    check_result('last-name', 'alert-success')
+    check_result('address', 'alert-success')
+    check_result('city', 'alert-success')
+    check_result('country', 'alert-success')
+    check_result('e-mail', 'alert-success')
+    check_result('phone', 'alert-success')
+    check_result('job-position', 'alert-success')
+    check_result('company', 'alert-success')
+    check_result('zip-code', 'alert-danger')
 
     driver.quit()
